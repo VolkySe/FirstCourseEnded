fc19824c740a8dbd7c63b8ec60edaeb5c204a058
VolkySe <sergej_lykov@mail.ru>
2024-11-25 11:53:10 +0300

    Class Employeer initialized

---
+++ src/Employeer.java
@@ -0,0 +1,14 @@
+public class Employeer {
+    private String lastName;
+    private String secondName;
+    private String patronymic;
+    private int department;
+    private int salary;
+
+    public static int id = 0;
+
+
+
+    }
+
+
