e9d73d06ebf22a71e1768cd87de70291d539657a
VolkySe <sergej_lykov@mail.ru>
2024-12-08 10:52:40 +0300

    Repeated basic level with tutor. Sout Employeer from array.

--- src/Employeer.java
+++ src/Employeer.java
@@ -1,14 +1,50 @@
 public class Employeer {
-    private String lastName;
-    private String secondName;
-    private String patronymic;
+    private static int idGen = 1;
+
+    private final int ID;
+    private final String FULLNAME;
     private int department;
     private int salary;
 
-    public static int id = 0;
 
-
-
+    public Employeer(String surname, String name, String patronymic, int department, int salary) {
+        ID = idGen++;
+        this.FULLNAME = surname + " " + name + " " + patronymic;
+        this.department = department;
+        this.salary = salary;
     }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public String getFULLNAME() {
+        return FULLNAME;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String toString() {
+        return "ID=" + ID +
+                ", FULLNAME='" + FULLNAME + '\'' +
+                ", department=" + department +
+                ", salary=" + salary;
+    }
+}
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world! ");
+
+    private final static Random RANDOM = new Random();
+    private final static String[] NAMES = {"Аарон", "Абдулла", "Абрам", "Аваз", "Августин", "Авраам", "Агап", "Агапит", "Агат", "Агафон", "Адам", "Адриан"};
+    private final static String[] SURNAMES = {"Смирнов", "Иванов", "Кузнецов", "Соколов", "Попов", "Лебедев", "Козлов", "Новиков", "Морозов", "Петров"};
+    private final static String[] PATRONYMIC = {"Абакумович", "Абрамович", "Абросимович", "Аввакумович", "Августович", "Авдеевич", "Авдиевич", "Авенирович", "Авериевич", "Аверкиевич"};
+
+    private static void initEmployers() {
+        for (int i = 0; i < EMPLOYEER.length; i++) {
+            EMPLOYEER[i] = new Employeer(
+                    SURNAMES[RANDOM.nextInt(0, SURNAMES.length)],
+                    NAMES[RANDOM.nextInt(0, NAMES.length)],
+                    PATRONYMIC[RANDOM.nextInt(0, PATRONYMIC.length)],
+                    RANDOM.nextInt(1, 6),
+                    RANDOM.nextInt(100_000, 500_000));
+        }
+    }
+
+    private final static Employeer[] EMPLOYEER = new Employeer[10];
+
+    public static void main(String[] args) {
+        initEmployers();
+        outputEmployers();
+    }
+
+    public static void outputEmployers() {
+        for (Employeer employeer : EMPLOYEER) {
+            System.out.println(employeer);
+        }
     }
 }
